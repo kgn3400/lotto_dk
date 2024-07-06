ef4a24b06df70a57db9691a2f9999ce1909f43a6
Kurt G. Nielsen <17993863+kgn3400@users.noreply.github.com>
2024-07-06 17:03:22 +0200

    Updated config_flow

--- custom_components/lotto_dk/config_flow.py
+++ custom_components/lotto_dk/config_flow.py
@@ -2,14 +2,18 @@
 
 from __future__ import annotations
 
-from typing import Any
+from collections.abc import Mapping
+from typing import Any, cast
 
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigEntry, ConfigFlow, OptionsFlow
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.data_entry_flow import FlowResult
-from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import config_validation as cv, selector
+from homeassistant.helpers.schema_config_entry_flow import (
+    SchemaCommonFlowHandler,
+    SchemaConfigFlowHandler,
+    SchemaFlowError,
+    SchemaFlowFormStep,
+)
 
 from .const import (
     CONF_EURO_JACKPOT,
@@ -19,148 +23,73 @@ from .const import (
     CONF_VIKING_LOTTO,
     DOMAIN,
     DOMAIN_NAME,
-    LOGGER,
 )
-
-#  from homeassistant import config_entries
-from .fix_entity_selector import EntitySelector, EntitySelectorConfig
 
 
 # ------------------------------------------------------------------
 async def _validate_input(
-    hass: HomeAssistant, data: dict[str, Any], errors: dict[str, str]
-) -> bool:
-    """Validate the user input allows us to connect."""
+    handler: SchemaCommonFlowHandler, user_input: dict[str, Any]
+) -> dict[str, Any]:
+    """Validate the user input."""
 
     if (
-        data[CONF_EURO_JACKPOT] is False
-        and data[CONF_LOTTO] is False
-        and data[CONF_VIKING_LOTTO] is False
+        user_input[CONF_EURO_JACKPOT] is False
+        and user_input[CONF_LOTTO] is False
+        and user_input[CONF_VIKING_LOTTO] is False
     ):
-        errors["base"] = "missing_selection"
-        return False
-
-    return True
+        raise SchemaFlowError("missing_selection")
+    return user_input
 
 
-# ------------------------------------------------------------------
-def _create_form(
-    user_input: dict[str, Any] | None = None,
-) -> vol.Schema:
-    """Create a form for step/option."""
+CONFIG_OPTIONS_SCHEMA = vol.Schema(
+    {
+        vol.Required(
+            CONF_EURO_JACKPOT,
+            default=True,
+        ): cv.boolean,
+        vol.Required(
+            CONF_LOTTO,
+            default=True,
+        ): cv.boolean,
+        vol.Required(
+            CONF_VIKING_LOTTO,
+            default=True,
+        ): cv.boolean,
+        vol.Optional(
+            CONF_LISTEN_TO_TIMER_TRIGGER,
+        ): selector.EntitySelector(
+            selector.EntitySelectorConfig(integration="timer", multiple=False),
+        ),
+        vol.Optional(
+            CONF_RESTART_TIMER,
+            default=False,
+        ): cv.boolean,
+    }
+)
+CONFIG_FLOW = {
+    "user": SchemaFlowFormStep(
+        CONFIG_OPTIONS_SCHEMA,
+        validate_user_input=_validate_input,
+    ),
+}
 
-    if user_input is None:
-        user_input = {}
-
-    return vol.Schema(
-        {
-            vol.Required(
-                CONF_EURO_JACKPOT,
-                default=user_input.get(CONF_EURO_JACKPOT, True),
-            ): cv.boolean,
-            vol.Required(
-                CONF_LOTTO, default=user_input.get(CONF_LOTTO, True)
-            ): cv.boolean,
-            vol.Required(
-                CONF_VIKING_LOTTO,
-                default=user_input.get(CONF_VIKING_LOTTO, True),
-            ): cv.boolean,
-            vol.Optional(
-                CONF_LISTEN_TO_TIMER_TRIGGER,
-                default=user_input.get(CONF_LISTEN_TO_TIMER_TRIGGER, ""),
-            ): EntitySelector(
-                EntitySelectorConfig(integration="timer", multiple=False),
-            ),
-            vol.Optional(
-                CONF_RESTART_TIMER,
-                default=user_input.get(CONF_RESTART_TIMER, False),
-            ): cv.boolean,
-        }
-    )
-
-
-# ------------------------------------------------------------------
-# ------------------------------------------------------------------
-class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Lotto DK."""
-
-    VERSION = 1
-
-    # ------------------------------------------------------------------
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle the initial step."""
-        errors: dict[str, str] = {}
-
-        await self.async_set_unique_id("device_unique_id_lotto_dk")
-        self._abort_if_unique_id_configured()
-
-        if user_input is not None:
-            try:
-                if await _validate_input(self.hass, user_input, errors):
-                    return self.async_create_entry(
-                        title=DOMAIN_NAME, data=user_input, options=user_input
-                    )
-
-            except Exception:  # pylint: disable=broad-except
-                LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-        else:
-            user_input = {}
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=_create_form(user_input),
-            errors=errors,
-        )
-
-    # ------------------------------------------------------------------
-    @staticmethod
-    @callback
-    def async_get_options_flow(
-        config_entry: ConfigEntry,
-    ) -> OptionsFlow:
-        """Get the options flow."""
-        return OptionsFlowHandler(config_entry)
+OPTIONS_FLOW = {
+    "init": SchemaFlowFormStep(
+        CONFIG_OPTIONS_SCHEMA,
+        validate_user_input=_validate_input,
+    ),
+}
 
 
 # ------------------------------------------------------------------
 # ------------------------------------------------------------------
-class OptionsFlowHandler(OptionsFlow):
-    """Options flow for Lotto DK."""
+class ConfigFlowHandler(SchemaConfigFlowHandler, domain=DOMAIN):
+    """Handle a config or options flow."""
 
-    def __init__(
-        self,
-        config_entry: ConfigEntry,
-    ) -> None:
-        """Initialize options flow."""
+    config_flow = CONFIG_FLOW
+    options_flow = OPTIONS_FLOW
 
-        self.config_entry = config_entry
+    def async_config_entry_title(self, options: Mapping[str, Any]) -> str:
+        """Return config entry title."""
 
-        self._selection: dict[str, Any] = {}
-        self._configs: dict[str, Any] = self.config_entry.data.copy()
-        self._options: dict[str, Any] = self.config_entry.options.copy()
-
-    # ------------------------------------------------------------------
-    async def async_step_init(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle the initial step."""
-        errors: dict[str, str] = {}
-
-        if user_input is not None:
-            try:
-                if await _validate_input(self.hass, user_input, errors):
-                    return self.async_create_entry(title=DOMAIN_NAME, data=user_input)
-            except Exception:  # pylint: disable=broad-except
-                LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-        else:
-            user_input = self._options.copy()
-
-        return self.async_show_form(
-            step_id="init",
-            data_schema=_create_form(user_input),
-            errors=errors,
-        )
+        return cast(str, DOMAIN_NAME)
